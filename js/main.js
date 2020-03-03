525fce7d537a707ec37349046d544c93d1f6a863
vadim.vilkhivskii <vadim.v.valki@gmail.com>
2020-03-03 15:41:52 +0200

    made footer and logical errors corrected

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-$(function() {
+$(function () {
     // Owl Carousel
     var owl = $(".owl-carousel");
     owl.owlCarousel({
-        items: 2,
+        responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        },
         margin: 10,
         loop: true,
         nav: true
